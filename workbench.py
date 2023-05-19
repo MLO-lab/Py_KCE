6798b23ce4e53c0e20cc37502fdcc1f0ea80f5dd
Sebastian G Gruber <33655584+SebGGruber@users.noreply.github.com>
2023-05-19 13:37:50 +0200

    Update workbench.py

--- workbench.py
+++ workbench.py
@@ -1,8 +1,8 @@
 for i in range(n):
-    for j in range(i, n):
+    for j in range(i+1, n):
         matrix[i, j] = calculate_SKCE_Matrix_Element(predictions[i], outcomes[i], predictions[j], outcomes[j], kernel)
 
 def helper_(tuple):
     i, j = tuple
     return calculate_SKCE_Matrix_Element(predictions[i], outcomes[i], predictions[j], outcomes[j], kernel)
-matrix = np.array(list(map(helper_, [[(i, j) for i in range(j, n)] for j in range(n)]))).reshape((n,n))
+matrix = np.array(list(map(helper_, [[(i, j) for i in range(j+1, n)] for j in range(n)]))).reshape((n,n))
